414732b8c0f24acce9354ea11219a2c5208552ef
ianmays <ian.mays@hotmail.com>
2026-05-05 18:11:29 +0100

    update build.bat to support test mode

--- build.bat
+++ build.bat
@@ -1,6 +1,19 @@
 @echo off
 call \watcom\owsetenv
-wcl -bt=dos -I. -Iinclude -Isrc -fe=dosmud.exe src\main.c src\game.c src\command.c src\world.c
+
+set FLAGS=-bt=dos -I. -Iinclude -Isrc
+
+if "%1"=="TEST_MODE" goto test
+
+goto build
+
+:test
+echo TEST MODE
+set FLAGS=%FLAGS% -DTEST_MODE
+
+:build
+wcl %FLAGS% -fe=dosmud.exe src\*.c
+
 echo.
 echo Compile finished. Press a key...
 pause
