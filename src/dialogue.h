b0ef99f73690c4cb1d71b684508c01be96c97f4e
ianmays <ian.mays@hotmail.com>
2026-05-25 17:59:40 +0100

    wrap dialogue header comment
    match dialogue.h module comment style to the rest of the PR

--- src/dialogue.h
+++ src/dialogue.h
@@ -3,7 +3,9 @@
 
 struct GameState;
 
-/* Dialogue helpers dispatch fixed NPC branches and one-off room hints. */
+/*
+ * Dialogue helpers dispatch fixed NPC branches and one-off room hints.
+ */
 
 /* NPC id for look HUD in the current room; 0 if none. */
 int npc_in_room(int room_id);
